fcba600f8766d8437e61625e54dac2cb9ce58248
harrrrshit <167506325+harrrrshit@users.noreply.github.com>
2024-08-08 11:00:58 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ RELEASE="$(rpm -E %fedora)"
 # this installs a package from fedora repos
 rpm-ostree install screen
 rpm-ostree install librewolf
+rpm-ostree install waydroid
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
